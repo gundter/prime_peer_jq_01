f1038c3c0c1d2eb42a0248edb85d127cb0f23d9b
Terry Gunderson <tgunderson25@gmail.com>
2015-04-20 13:36:18 -0500

    first commit

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -3,3 +3,13 @@
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+var i;
+var array = [];
+
+var Person = {
+	age: randomNumber(1, 100),
+	weight: randomNumber(1, 100),
+	sex: function (){
+		
+	}
+}
