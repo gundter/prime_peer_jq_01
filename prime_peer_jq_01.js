ade3ac296e93a5f28b54a43965abac7b44e8be73
Vince Jones <vincethebutcher@gmail.com>
2015-04-20 14:53:24 -0500

    Functionality fix

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -4,12 +4,20 @@ function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
 var i;
-var array = [];
 
-var Person = {
-	age: randomNumber(1, 100),
-	weight: randomNumber(1, 100),
-	sex: function (){
-		
-	}
+function Person() {
+	this.age = randomNumber(1, 100);
+	this.weight = randomNumber(1, 100);
+	this.sex = function (){
+		num = randomNumber(1, 2);
+		return this.sexType = num == 1 ? "Male" : "Female";
+	};
 }
+
+$(document).ready(function (){
+	$("#makePeople").on("click", function() {
+		var person1 = new Person();
+		person1.sex();
+			$("#personTable").prepend("<tr><td>Age: " + person1.age + "</td><td>Weight: " + person1.weight + "</td><td>Sex: " + person1.sexType + "</td></tr><br>");
+	});
+});
