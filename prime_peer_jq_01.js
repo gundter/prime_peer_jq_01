f13d2e97a1bcacc587494ab75839551a1c3b0813
Vince Jones <vincethebutcher@gmail.com>
2015-04-20 15:08:52 -0500

    Add male and female names

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -4,20 +4,31 @@ function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
 var i;
+var femaleNames = [ "Cici", "Estelle", "Hazel", "Jodie", "Jules", "Immy", "Doris", "Fion", "Gabriella", "Ester" ];
+var maleNames = [ "Niko", "Rufus", "Rex", "Herman", "Iggy", "Jett", "Link", "Zeke", "Zuriel", "Tex" ];
 
 function Person() {
 	this.age = randomNumber(1, 100);
 	this.weight = randomNumber(1, 100);
-	this.sex = function (){
+	this.sex = function() {
 		num = randomNumber(1, 2);
 		return this.sexType = num == 1 ? "Male" : "Female";
 	};
+	this.name = function() {
+		if (this.sexType == "Male") {
+			var nameNum = randomNumber(0, 9);
+			return maleNames[nameNum];
+		} else {
+			var nameNum = randomNumber(0, 9);
+			return femaleNames[nameNum];
+		}
+	}
 }
 
 $(document).ready(function (){
 	$("#makePeople").on("click", function() {
 		var person1 = new Person();
 		person1.sex();
-			$("#personTable").prepend("<tr><td>Age: " + person1.age + "</td><td>Weight: " + person1.weight + "</td><td>Sex: " + person1.sexType + "</td></tr><br>");
+			$("#personTable").prepend("<tr><td>Name: " + person1.name() +"</td><td>Age: " + person1.age + "</td><td>Weight: " + person1.weight + "</td><td>Sex: " + person1.sexType + "</td></tr><br>");
 	});
 });
